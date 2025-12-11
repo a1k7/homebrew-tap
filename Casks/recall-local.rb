299db4d56607e87d3bdecafdd03bb165f997c97d
a1k7 <138591441+a1k7@users.noreply.github.com>
2025-12-11 12:13:14 +0530

    Update URL for Recall Local to Corporate Brain

--- Casks/recall-local.rb
+++ Casks/recall-local.rb
@@ -3,7 +3,7 @@ cask "recall-local" do
   sha256 "449ee3589e391ce9bfc7528f345e9c85610b2dd282aca88d6f4faf8012c4b1e8  "
 
   # Update this to match your username and repo name exactly
-  url "https://github.com/a1k7/Recall-Local/releases/download/v#{version}/RecallLocal.zip"
+  url "https://github.com/a1k7/Corporate-Brain/releases/download/v1.0/SmartSortLocal.zip"
   name "Recall Local"
   desc "Local AI file organizer and chat powered by Llama 3"
   homepage "https://github.com/a1k7/Recall-Local"
